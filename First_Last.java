4ce62ef7da32daad33d497cddb7bc84d9f22d982
Shashwat Dwivedi <117923310+Shashwatdwivedi30@users.noreply.github.com>
2023-08-06 12:47:53 +0530

    Create First_Last.java

---
+++ First_Last.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class First_Last
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int first_digit = 0, last_digit = 0, n;
+        System.out.println("Enter the number:");
+        n = sc.nextInt();
+        last_digit = n % 10;
+        System.out.println("Last digit: " + last_digit);
+        while (n != 0)
+          {
+              first_digit = n % 10;
+              n /= 10;
+          }
+        System.out.println("First digit:" + first_digit);
+    }
+}
