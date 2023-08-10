212ee17e5bc4946f26d683c6b492131966e6fccf
Shashwat Dwivedi <117923310+Shashwatdwivedi30@users.noreply.github.com>
2023-08-10 23:53:13 +0530

    Create Table.java

---
+++ Table.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Table {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,i;
+        System.out.println("Enter a number");
+        a=sc.nextInt();
+        for(i=1;i<=10;i++){
+            System.out.println(a+"*"+i+"="+a*i);
+        }
+
+    }
+}
+//Shashwat Dwivedi
+//62
