4c2ef983ab4f01d6d5f3939c6c5f3255f0c03d54
Shashwat Dwivedi <117923310+Shashwatdwivedi30@users.noreply.github.com>
2023-08-10 23:38:30 +0530

    Create Natural_nos.java

---
+++ Natural_nos.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Natural_nos {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,sum=0;
+        System.out.println("Enter a number");
+        n=sc.nextInt();
+        for(i=1;i<=n;i++){
+            sum+=i;
+        }
+        System.out.println(sum);
+    }
+}
+//Shashwat Dwivedi
+//62        
